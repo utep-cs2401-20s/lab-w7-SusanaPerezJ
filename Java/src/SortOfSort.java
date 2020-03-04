897b42bf4fb5a96fa25c314f479f6a7a505eda44
Susana Perez Juarez <sperezjuar1@miners.utep.edu>
2020-03-04 14:48:38 -0700

    Added loops find the largest element and swap it.

--- Java/src/SortOfSort.java
+++ Java/src/SortOfSort.java
@@ -1,26 +1,70 @@
+import java.util.Arrays;
+
 public class SortOfSort {
-    public static void sortOfSort(int[] arr){
-        /*int place = arr.length-1;
-        int scope = arr.length-1;
-        //stopping point at length-1
-        for(int i = 0, j = scope; i<scope; i++){
-            //find the largest
-            int largest = arr[i];
-            int temp = 0;
-            if(arr[i] > arr[i+1]){
-                temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+    public static void main(String[] args) {
+        int[] a = {3,2,1,5,6,8,0,3};
+        newSort(a);
+    }
+    public static void sortOfSort(int[] arr) {
+        int countLeft = 2;
+        int countRight = 2;
+        int leftIndex = 0;
+        int rightIndex = arr.length - 1;
+        int times = 2;
+       while (times > 0) {
+            //get the largest at the end
+            while (countRight > 0) {
+                int temp = 0;
+                for (int i = leftIndex, j = rightIndex; j >= 0; i++, j--) {
+                    if (arr[i] > arr[j]) {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+                countRight--;
+                countLeft = 2;
             }
-        }*/
-        //get the largest at the end
-        int temp = 0;
-        for(int i = 0, j = arr.length-1; j >= 0; i++, j--){
-            if(arr[i] > arr[j]){
-                temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+            //get the largest at the beginning
+            while (countLeft > 0) {
+                int temp2 = 0;
+                for (int i = rightIndex, j = leftIndex; j >= 0; i++, j--) {
+                    if (arr[i] > arr[j]) {
+                        temp2 = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp2;
+                    }
+                }
+                countLeft--;
+                countRight = 2;
             }
+            times--;
         }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void newSort(int[]a) {
+        int temp;
+        int first = 0;
+        int second = 1;
+        int counter = 0;
+        int scope = a.length;
+        //gets the biggest element to the las position
+        while (counter < 2) {
+            while (second < scope) {
+                if (a[first] > a[second]) {
+                    temp = a[first];
+                    a[first] = a[second];
+                    a[second] = temp;
+                }
+                first++;
+                second++;
+            }
+            first = 0;
+            second = 1;
+            scope--;
+            counter++;
+        }
+        System.out.println(scope);
+        System.out.println(Arrays.toString(a));
     }
 }
